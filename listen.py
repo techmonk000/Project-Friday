99e7119d00fbdbbb80293da400f85d6716bba220
techmonk000 <swarnavomukherjee03@gmail.com>
2023-10-13 00:01:01 +0530

    listen-changes

--- listen.py
+++ listen.py
@@ -1,24 +1,40 @@
-import speech_recognition as sr # importing the speech_recognition library
+import speech_recognition as sr #pip install speechrecognition
+from googletrans import Translator #pip install googletrans==3.1.0a0
+
+# 1 - Listen : Hindi or English
 
 def Listen():
 
     r = sr.Recognizer()
 
-    with sr.Microphone() as source: # using our own desktop microphone as source of voice
-        print("Interpretaing and trying to analyse your voice ...")
-        r.energy_threshold = 4000
-        audio = r.listen(source,0,2.5) # making it such that after 2.5 seconds it stops listening to your voice
-
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source,0,8) # Listening Mode.....
+    
     try:
-        print("Recognizing....")
-        query = r.recognize_google(audio , language="en-in") # it is using google recogniser to catch the audio
-        print(f"Sir, Your command was: {query}")
+        print("Recognizing...")
+        query = r.recognize_google(audio,language="en-in")
 
     except:
-        return "There was an error ! "
+        return ""
     
-    
-    query = str(query)
-    return query.lower() # it stores the command in lower case
+    query = str(query).lower()
+    return query
 
-Listen()
+# 2 - Translation
+
+def TranslationHinToEng(Text):
+    line = str(Text)
+    translate = Translator()
+    result = translate.translate(line)
+    data = result.text
+    print(f"You : {data}.")
+    return data
+
+# 3 - Connect
+
+def MicExecution():
+    query = Listen()
+    data = TranslationHinToEng(query)
+    return data
