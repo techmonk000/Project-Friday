3dfedb9926c5b3cf81f19bbb5d081d375a6b1383
techmonk000 <swarnavomukherjee03@gmail.com>
2023-10-12 23:58:58 +0530

    changes

--- speak.py
+++ speak.py
@@ -1,17 +1,12 @@
-from gtts import gTTS # importing the gtts package 
-import os
-import time 
-def say(text, lang='en'): # creating a function for the ai to talk
-    try:
-        tts = gTTS(text=text, lang=lang) 
-        tts.save("output.mp3")
-        time.sleep(1)
-        os.system("mpg123 output.mp3")  # storing the output voice as a mp3 file
-        os.remove("output.mp3")
-        print("  ")
-        print(f"Friday: {text}")# printing the speech said by ariel to make it easier to understand
-        print("  ")
-    except Exception as e:
-        print(f"An error has occurred: {e}")
+import pyttsx3
 
-# Build by Swarnavo Mukherjee . This is the start of the project
+def Speak(Text):
+    engine = pyttsx3.init("sapi5")
+    voices = engine.getProperty('voices')
+    engine.setProperty('voices',voices[1].id)
+    engine.setProperty('rate',170)
+    print("")
+    print(f"You : {Text}.")
+    print("")
+    engine.say(Text)
+    engine.runAndWait()
